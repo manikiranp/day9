e05d53bca35bbebf3370522a3cc350185b9f8a9e
manikiranp <manikiranp3@gmail.com>
2020-10-08 20:49:39 +0530

    dailywages

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,9 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "Welcome to Employee Wage Computation Program"
+
+wage_per_hr=20
+working_hrs=8
 x=$(($RANDOM%2))
+dailywage=$(( $wage_per_hr * $working_hrs ))
 if [ $x -eq 1 ]; then
 	echo "Employee is present"
+	echo "Daily Employee wage: $dailywage"
 else
 	echo "Employee is absent"
+	echo "Daily Employee wage: 0"
+fi
 
