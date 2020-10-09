bc0693ad9189212842ec7ebaef29875749b681b5
manikiranp <manikiranp3@gmail.com>
2020-10-09 10:38:22 +0530

    using switch case

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,25 +1,34 @@
 #!/bin/bash 
 
 echo "Welcome to Employee Wage Computation Program"
-
+parttime=1
+fulltime=2
 wage_per_hr=20
 function wage() {
 echo $(($1*wage_per_hr))
 }
-x=$(($RANDOM%3))
-if [ $x -eq 1 ]; then
-	work_hrs=8
-	echo "Employee is present"
-	echo "Employee daily wage: $(wage $work_hrs)"
-elif [ $x -eq 0 ]; then
-	work_hrs=0
-	echo "Employee is absent"
-	echo "Employee daily wage: $(wage $work_hrs)"
-else
-	work_hrs=4
-	echo "Employee is present part-time"
-	echo "Employee daily wage: $(wage $work_hrs)"
-fi
+function getworkhrs() {
+        case $1 in
+                $parttime)
+                        work_hrs=4
+			echo "Employee is present part-time"
+        		echo "Employee daily wage: $(wage $work_hrs)"
+                        ;;
+                $fulltime)
+			work_hrs=8
+                        echo "Employee is present"
+        		echo "Employee daily wage: $(wage $work_hrs)"
+                        ;;
+                *)
+                        work_hrs=0
+        		echo "Employee is absent"
+	        	echo "Employee daily wage: $(wage $work_hrs)"
+                        ;;
+        esac
+}
+empcheck=$(($RANDOM%3))
+getworkhrs $empcheck
+
 
 
 
