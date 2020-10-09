e1e95c7520982f543e13c01d38138a37e84083a5
manikiranp <manikiranp3@gmail.com>
2020-10-09 10:27:00 +0530

    parttime

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,16 +1,25 @@
-#!/bin/bash
+#!/bin/bash 
 
 echo "Welcome to Employee Wage Computation Program"
 
 wage_per_hr=20
-working_hrs=8
-x=$(($RANDOM%2))
-dailywage=$(( $wage_per_hr * $working_hrs ))
+function wage() {
+echo $(($1*wage_per_hr))
+}
+x=$(($RANDOM%3))
 if [ $x -eq 1 ]; then
+	work_hrs=8
 	echo "Employee is present"
-	echo "Daily Employee wage: $dailywage"
-else
+	echo "Employee daily wage: $(wage $work_hrs)"
+elif [ $x -eq 0 ]; then
+	work_hrs=0
 	echo "Employee is absent"
-	echo "Daily Employee wage: 0"
+	echo "Employee daily wage: $(wage $work_hrs)"
+else
+	work_hrs=4
+	echo "Employee is present part-time"
+	echo "Employee daily wage: $(wage $work_hrs)"
 fi
+
+
 
